fb5bb8ec30754e2873c8d7a0df5e02309337abd8
Larina <49904492+Larinax999@users.noreply.github.com>
2023-09-09 19:59:32 +0700

    ✨ 1.0

---
+++ ClearCache.bat
@@ -0,0 +1,4 @@
+@echo off
+rmdir /q /s .\CFXMain\cache
+echo OK
+timeout 2
---
+++ Start.bat
@@ -0,0 +1,3 @@
+@echo off
+"./Bin/FXServer.exe" +set serverProfile "Main" +set onesync on +set onesync_enableInfinity 1
+pause
